34f688fdb573dc0a57492d3dfe5e38ca5e602be6
Faiz Abbas <faiz_abbas@ti.com>
2019-03-08 15:33:53 +0530

    board: ti: am335x: Add platdata for ethernet
    The SPL image overflows when cpsw dt nodes are added and SPL_OF_CONTROL
    is enabled. Use platdata instead to save space.
    
    Signed-off-by: Faiz Abbas <faiz_abbas@ti.com>

--- drivers/net/ti/cpsw.c
+++ drivers/net/ti/cpsw.c
@@ -34,42 +34,6 @@ DECLARE_GLOBAL_DATA_PTR;
 #define FULLDUPLEXEN		BIT(0)
 #define MIIEN			BIT(15)
 
-/* reg offset */
-#define CPSW_HOST_PORT_OFFSET	0x108
-#define CPSW_SLAVE0_OFFSET	0x208
-#define CPSW_SLAVE1_OFFSET	0x308
-#define CPSW_SLAVE_SIZE		0x100
-#define CPSW_CPDMA_OFFSET	0x800
-#define CPSW_HW_STATS		0x900
-#define CPSW_STATERAM_OFFSET	0xa00
-#define CPSW_CPTS_OFFSET	0xc00
-#define CPSW_ALE_OFFSET		0xd00
-#define CPSW_SLIVER0_OFFSET	0xd80
-#define CPSW_SLIVER1_OFFSET	0xdc0
-#define CPSW_BD_OFFSET		0x2000
-#define CPSW_MDIO_DIV		0xff
-
-#define AM335X_GMII_SEL_OFFSET	0x630
-
-/* DMA Registers */
-#define CPDMA_TXCONTROL		0x004
-#define CPDMA_RXCONTROL		0x014
-#define CPDMA_SOFTRESET		0x01c
-#define CPDMA_RXFREE		0x0e0
-#define CPDMA_TXHDP_VER1	0x100
-#define CPDMA_TXHDP_VER2	0x200
-#define CPDMA_RXHDP_VER1	0x120
-#define CPDMA_RXHDP_VER2	0x220
-#define CPDMA_TXCP_VER1		0x140
-#define CPDMA_TXCP_VER2		0x240
-#define CPDMA_RXCP_VER1		0x160
-#define CPDMA_RXCP_VER2		0x260
-
-/* Descriptor mode bits */
-#define CPDMA_DESC_SOP		BIT(31)
-#define CPDMA_DESC_EOP		BIT(30)
-#define CPDMA_DESC_OWNER	BIT(29)
-#define CPDMA_DESC_EOQ		BIT(28)
 
 /*
  * This timeout definition is a worst-case ultra defensive measure against
